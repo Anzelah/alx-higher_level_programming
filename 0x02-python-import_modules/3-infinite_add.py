6d359cdb4348ca3f0411470f717711951e2a4097
Anzelah <jepkoechanzellah@gmail.com>
2023-06-14 13:14:04 -0700

    set name to main to make it usable as script

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-
-result = 0
-for i in range(len(argv) - 1):
-    result += int(argv[i + 1])
-
-print(result)
-
 if __name__ == "__main__":
+    from sys import argv
+
+    result = 0
+    for i in range(len(argv) - 1):
+        result += int(argv[i + 1])
+    print(result)
