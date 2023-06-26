43b2024e422d54f109b8ac3beabf64686b857536
Anzelah <jepkoechanzellah@gmail.com>
2023-06-26 07:51:45 -0700

    print all elements in a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        length = 0
+        for i in my_list:
+            if length < x:
+                print(i, end='')
+                length += 1
+        print()
+        return (length)
+    except:
+        print("An unknown error")
+
