0c60cfb4c43443830f97060fc4278b9dc0865a9e
Anzelah <jepkoechanzellah@gmail.com>
2023-06-27 15:36:42 -0700

    retrieve the property and set it

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a class square."""
+
+
+class Square:
+    """ Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes the square.
+        Define the square attributes/variables."""
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of square."""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """ Define the square capabilities."""
+        return self.__size * self.__size
