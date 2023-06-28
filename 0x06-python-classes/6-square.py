f39daa63d4bf245ff12c3b7bc153a4e37f34f5c6
Anzelah <jepkoechanzellah@gmail.com>
2023-06-27 17:18:50 -0700

    print coordinates with a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Defines a class square."""
+
+
+class Square:
+    """ Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the square.
+        Define the square attributes/variables."""
+
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Retrieve the size of square."""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def position(self):
+        """Retrieve the size of square."""
+
+        return position.__size
+
+    @position.setter
+    def position(self, value):
+        """Set the position attribute of the square."""
+
+        self.__position = value
+        if not isinstance(value, int):
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+    def area(self):
+        """ Define the square capabilities."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print square to stdout with character #"""
+
+        if self.__size == 0:
+            print()
+        for _ in range(self.__position[1]):
+            print()
+        for _ in range(self.__size):
+            print(' ' * self.__position[0] + '#' * self.__size)
