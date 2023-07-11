f8018b78e88dae25b1635cdcebe226f5ebcd5ebb
Anzelah <jepkoechanzellah@gmail.com>
2023-07-11 15:32:13 -0700

    from json string to object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a module."""
+
+import json
+"""Imports a module."""
+
+
+def from_json_string(my_str):
+    """A function to convert from json string to object."""
+    return json.loads(my_str)
