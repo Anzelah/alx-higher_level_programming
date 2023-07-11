fd70d07e0bddf15aa0336b74bddc2c9840a7f1b5
Anzelah <jepkoechanzellah@gmail.com>
2023-07-11 14:18:18 -0700

    add documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Defines the module."""
+
+
 def read_file(filename=""):
+    """Function for reading the file."""
     with open('my_file_0.txt', encoding="utf-8") as f:
         print(f.read(), end="")
