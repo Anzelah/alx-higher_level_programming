6b407f14f44e239e22b4bd67df836df8d23e4292
Anzelah <jepkoechanzellah@gmail.com>
2023-07-11 15:22:39 -0700

    return the json representative

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a module."""
+
+import json
+"""Imports a module."""
+
+
+def to_json_string(my_obj):
+    """A function that returns a json representation."""
+    json.dumps(my_obj)
