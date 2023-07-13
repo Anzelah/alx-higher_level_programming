8b361798c1a5b795b8ce87009af03cc32d622df1
Anzelah <jepkoechanzellah@gmail.com>
2023-07-13 10:35:21 -0700

    revert changes and use write than append

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,7 +7,7 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, encoding="utf-8") as f:
         get_line = f.readlines()
 
-    with open(filename, 'a', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         for line in get_line:
             f.write(line)
             if search_string in line:
