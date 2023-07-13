cad971a9621a0820458828557053516dd8995411
Anzelah <jepkoechanzellah@gmail.com>
2023-07-13 10:28:29 -0700

    change mode from append to write

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,10 +4,11 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """Function to insert a text after a specific string."""
-    with open(filename, encoding="utf=8") as f:
+    with open(filename, encoding="utf-8") as f:
         get_line = f.readlines()
 
-    with open(filename, 'a', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         for line in get_line:
-            if search_string:
+            f.write(line)
+            if search_string in line:
                 f.write(new_string)
