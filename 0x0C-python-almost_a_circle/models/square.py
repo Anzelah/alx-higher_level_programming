6130d1c1c1896a7051c83fa241e80a00da0a0f71
Anzelah <jepkoechanzellah@gmail.com>
2023-07-16 06:24:09 -0700

    update width and height to size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,17 @@ class Square(Rectangle):
         """Initialize the subclass."""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Retrieve the width of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the width of the square"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Overide the str method."""
         return ("Square] ({}) {}/{} - {}" .format(self.id, self.x,
