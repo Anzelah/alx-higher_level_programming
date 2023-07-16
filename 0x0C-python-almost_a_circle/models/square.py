a23a495160802d477b8f639de430796781fc6773
Anzelah <jepkoechanzellah@gmail.com>
2023-07-16 06:32:28 -0700

    assign attributes to arguments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,5 +22,27 @@ class Square(Rectangle):
 
     def __str__(self):
         """Overide the str method."""
-        return ("Square] ({}) {}/{} - {}" .format(self.id, self.x,
+        return ("[Square] ({}) {}/{} - {}" .format(self.id, self.x,
                 self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """Assign an argument to each attribute."""
+        if args and len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                if key == 'size':
+                    self.size = value
+                if key == 'x':
+                    self.x = value
+                if key == 'y':
+                    self.y = value
