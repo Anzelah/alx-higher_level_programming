83bd90c1fb08018d837450e7fcdd276f1167c488
Anzelah <jepkoechanzellah@gmail.com>
2023-07-24 10:55:22 -0700

    create  anew class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Import a module."""
+
+
+class Rectangle(BaseGeometry):
+    """Create a new class inheriting from basegeometry."""
+
+    def __init__(self, width, height):
+        """Initialize the class."""
+
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
