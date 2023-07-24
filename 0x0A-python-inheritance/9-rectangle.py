b0de29161bd02279c596796574173487cb4c8401
Anzelah <jepkoechanzellah@gmail.com>
2023-07-24 12:44:19 -0700

    add str method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a module."""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Import a module."""
+
+
+class Rectangle(BaseGeometry):
+    """Create a new class inheriting from basegeometry."""
+
+    def __init__(self, width, height):
+        """Initialize the class."""
+
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}" .format(self.__width, self.__height))
+
+    def area(self):
+        print(self.__width * self.__height)
