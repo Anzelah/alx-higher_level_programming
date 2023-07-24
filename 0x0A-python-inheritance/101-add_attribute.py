f610e6647ab6872ba6cdeac13de17a88af8bf6b0
Anzelah <jepkoechanzellah@gmail.com>
2023-07-24 13:58:32 -0700

    adding documentation

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Defines a module."""
+
 
 def add_attribute(obj, name, value):
+    """Function that adds an attribute."""
 
     if hasattr(obj, '__dict__'):
         setattr(obj, name, value)
