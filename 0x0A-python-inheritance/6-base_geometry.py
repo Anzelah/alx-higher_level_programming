84b5f191227b0b580910bb81850c4b3604da1212
Anzelah <jepkoechanzellah@gmail.com>
2023-07-24 10:18:44 -0700

    add an area method to previous class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a module."""
+
+
+class BaseGeometry:
+    """Create a new class."""
+
+    def __init__(self):
+        """Initialize the class."""
+        pass
+
+    def area(self):
+        """Calculate the area."""
+        raise Exception('area() is not implemented')
