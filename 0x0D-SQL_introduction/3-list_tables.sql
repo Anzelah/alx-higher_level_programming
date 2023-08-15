9ea03bede3fa694f442551de98c4a297473f5bb7
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 07:59:20 -0700

    order the output

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
 -- list all the tables of a database my MySQL server
-SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_type = 'BASE TABLE'
+SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_type = 'BASE TABLE' ORDER BY table_name ASC;
