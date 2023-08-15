36c696bc42ac7d3cada089da25edb60d8e680356
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 15:25:42 -0700

    syntax error

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,5 @@
 -- list all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
 SELECT show, name
 FROM second_table
-WHERE EXISTS
-(SELECT name FROM second_table)
+WHERE name IS NOT NULL
 ORDER BY score DESC;
