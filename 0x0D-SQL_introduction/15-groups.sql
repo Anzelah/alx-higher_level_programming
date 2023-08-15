e992154f7dc93b2f9d6a351e47a105169cb17018
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 15:04:18 -0700

    use order by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,9 +1,6 @@
 -- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in my MySQL server.
-SELECT COUNT(score) AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
+GROUP BY score
 HAVING COUNT(score) > 1
-ORDER BY (
-	SELECT COUNT(score)
-	FROM second_table
-	HAVING COUNT(score) > 1
-) DESC;
+ORDER BY score DESC;
