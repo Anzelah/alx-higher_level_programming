f52d2fbd2375e1e3152c4c1889b94b16c656bb49
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 15:55:14 -0700

    average temperture

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- display the average temperature (Fahrenheit) by city ordered by temperature (descending).
+curl -O url
+mysql -u root -p hbtn_0c_0 < sql_file
+SELECT city, AVG(*)
+FROM temperatures
+ORDER BY temperatures DESC;
