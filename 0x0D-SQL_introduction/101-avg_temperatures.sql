03823b105f6b49ba0b798afd0ec8c672de5db4b0
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 16:02:17 -0700

    include group by

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,4 @@
 -- display the average temperature (Fahrenheit) by city ordered by temperature (descending).
-curl -O url
-mysql -u root -p hbtn_0c_0 < sql_file
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
