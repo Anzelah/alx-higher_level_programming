9d187deb4686e5f1eff0c8a50ea8998af45786cc
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 15:59:37 -0700

    include an AS command

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- display the average temperature (Fahrenheit) by city ordered by temperature (descending).
 curl -O url
 mysql -u root -p hbtn_0c_0 < sql_file
-SELECT city, AVG(value)
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-ORDER BY temperatures DESC;
+ORDER BY avg_temp DESC;
