901eda450937ef9312f4c50d87ae805d331fe259
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 07:13:26 -0700

    use if not exists command

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,10 +1,2 @@
 -- create a database in MySQL server
-CREATE DATABASE hbtn_0c_0;
-IF DB_ID('hbtn_0c_0') IS NULL
-BEGIN
-	RETURN 0;
-END
-ELSE
-BEGIN
-	RETURN 1;
-END
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
