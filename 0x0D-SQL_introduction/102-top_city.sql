ef8d72109235497f0f7ee9c1b8bacc09b122d588
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 16:13:09 -0700

    use bracket in group

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,5 @@
 -- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 SELECT city, avg(value) AS avg_temp
 FROM temperatures
-WHERE month = (7, 8)
-GROUP BY city
+WHERE month IN (7, 8)
 ORDER BY avg_temp DESC LIMIT 3;
