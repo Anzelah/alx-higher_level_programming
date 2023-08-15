10a77279099cb1eba6d3050b83264d494d86f191
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 15:41:57 -0700

    fixed syntax error

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- list all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
 -- Don’t list rows without a name value
-SELECT show, name
+SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
