475636747a8cd5dd988a35386b06fe0ee7e51ecb
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 14:55:15 -0700

    number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in my MySQL server.
-SELECT COUNT(score) AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
 HAVING COUNT(score) > 1
 ORDER BY number DESC;
