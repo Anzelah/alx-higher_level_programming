1228e9600bf9649bd6f0c21e982ad38804a903d1
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 15:17:32 -0700

    add subquery

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,6 @@
 -- list all records of the table second_table of the database hbtn_0c_0 in my MySQL server
 SELECT show, name
 FROM second_table
-WHERE name EXISTS
+WHERE EXISTS
+( SELECT name FROM second_table)
 ORDER BY score DESC;
