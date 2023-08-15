1202a9ce9f838519be804c968aaf019fae1353a1
Anzelah <jepkoechanzellah@gmail.com>
2023-08-15 15:27:17 -0700

    fixing reuquirement error

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- list all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
+-- Don’t list rows without a name value
 SELECT show, name
 FROM second_table
 WHERE name IS NOT NULL
