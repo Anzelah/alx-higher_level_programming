a97a12a0227ad4f32ca19f71edca7bc3f91c6eec
Anzelah <jepkoechanzellah@gmail.com>
2023-08-16 10:52:15 -0700

    add storage engines

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(state_id);
+	state_id INT NOT NULL FOREIGN KEY(id) REFERENCES states(id);
 	name VARCHAR(256) NOT NULL
-);
+) type=InnoDB;
