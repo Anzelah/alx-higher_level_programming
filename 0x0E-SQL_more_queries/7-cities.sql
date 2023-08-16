2002e8e0f1fce7185bb061440aa6a0192cf591f6
Anzelah <jepkoechanzellah@gmail.com>
2023-08-16 10:39:08 -0700

    fix syntax

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES states(state_id);
+	state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(state_id);
 	name VARCHAR(256) NOT NULL
 );
