a0d4602d153239e37c5c09e70fe802cefc673281
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 01:56:47 -0700

    fix syntax error

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT s.title, g.genre_id
-FROM tv_shows as s LEFT JOIN tv_show_genres as g
+FROM tv_shows AS s LEFT JOIN tv_show_genres AS g
 ON s.id = g.show_id
 WHERE g.genre_id IS NULL
 ORDER BY s.title, g.genre_id;
