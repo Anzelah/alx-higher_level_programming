576bd41d953447acbd03065e0b6f6eae3f137708
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 00:37:16 -0700

    use an alias with ifnull

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- If a show doesn’t have a genre, display NULL
-SELECT s.title, IFNULL(g.genre_id, 'NULL')
+SELECT s.title, IFNULL(g.genre_id, 'NULL') AS genre_id
 FROM tv_shows AS s LEFT JOIN tv_show_genres AS g
 ON s.id = g.show_id
 ORDER BY s.title, g.genre_id;
