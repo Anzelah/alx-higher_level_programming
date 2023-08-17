019f019911f7fdb08c433e39db525880358e2e15
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 10:25:02 -0700

    couldn't hack it. Keeg getting table doesnt exit error

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,3 @@
 -- list all shows from hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_shows.title - rating sum
-SELECT name, SUM(rate) AS rating
-FROM tv_genres AS g
-INNER JOIN tv_show_genres AS sg
-ON sg.genre_id = g.id
 
-INNER JOIN tv_show_ratings AS r
-ON r.show_id = sg.show_id
-GROUP BY name
-ORDER BY rating DESC;
