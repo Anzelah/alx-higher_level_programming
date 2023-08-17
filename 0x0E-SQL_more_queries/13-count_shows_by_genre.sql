056557683629d85e84806d6e61ff0fb44db5b816
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 03:21:36 -0700

    column title should be genre, not genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
 -- First column must be called genre
 -- Second column must be called number_of_shows
-SELECT s.name AS genres, COUNT(g.genre_id) AS number_of_shows
+SELECT s.name AS genre, COUNT(g.genre_id) AS number_of_shows
 FROM tv_genres AS s LEFT JOIN tv_show_genres AS g
 ON s.id = g.genre_id
 GROUP BY s.name
