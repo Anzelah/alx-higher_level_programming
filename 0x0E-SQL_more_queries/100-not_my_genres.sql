ebbbe9c3c25daa3c99648b6dd982bebc6bccb286
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 07:52:14 -0700

    trying something

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,11 +2,6 @@
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
 -- Each record should display: tv_genres.name
 SELECT DISTINCT g.name FROM tv_genres AS g
-INNER JOIN tv_show_genres AS sg
-ON g.id = sg.genre_id
-
-INNER JOIN tv_shows AS s
-ON s.id = sg.show_id
 WHERE g.name NOT IN (
 	SELECT name FROM tv_genres AS g
 	INNER JOIN tv_show_genres AS sg
