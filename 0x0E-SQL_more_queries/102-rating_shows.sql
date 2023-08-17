7e2753a1af2f82e68abbb5fcfffa1df90ceb9b4a
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 10:01:41 -0700

    fix syntax

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,11 @@
 -- list all shows from hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_shows.title - rating sum
-SELECT g.name, SUM(rate) AS rating
+SELECT name, SUM(rate) AS rating
 FROM tv_genres AS g
 INNER JOIN tv_show_genres AS sg
 ON g.id = sg.genre_id
 
 INNER JOIN tv_show_ratings AS r
 ON sg.show_id = r.show_id
-GROUP BY g.name
+GROUP BY name
 ORDER BY rating DESC;
