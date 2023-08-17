74ea09b8ffd6168fc74e28c56b359ba03fa57473
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 03:15:54 -0700

    count number of shows line=ked by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- list all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- First column must be called genre
+-- Second column must be called number_of_shows
+SELECT s.name AS genres, COUNT(g.genre_id) AS number_of_shows
+FROM tv_genres AS s LEFT JOIN tv_show_genres AS g
+ON s.id = g.genre_id
+GROUP BY name
+ORDER BY number_of_shows DESC;
