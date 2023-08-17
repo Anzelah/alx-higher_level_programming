e7cbd213615c5563e43ac38862199893ec7eb17c
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 09:59:56 -0700

    list shows by rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,11 @@
 -- list all shows from hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_shows.title - rating sum
-SELECT s.title, SUM(r.show_id) AS rating
-FROM tv_shows AS s
+SELECT g.name, SUM(rate) AS rating
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS sg
+ON g.id = sg.genre_id
 
 INNER JOIN tv_show_ratings AS r
-ON r.show_id = s.id
-GROUP BY s.title
+ON sg.show_id = r.show_id
+GROUP BY g.name
 ORDER BY rating DESC;
