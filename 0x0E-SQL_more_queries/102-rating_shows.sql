0a5fc6e262ee4bdba8486196d68a9012ed1250b8
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 10:30:05 -0700

    last trial

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,3 +1,9 @@
 -- list all shows from hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_shows.title - rating sum
+SELECT s.title, SUM(rate) AS rating
+FROM tv_shows AS s
 
+INNER JOIN tv_show_ratings AS r
+ON r.show_id = s.id
+GROUP BY s.title
+ORDER BY rating DESC;
