394bf497c15dad7583a708ed071b841554326e1a
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 03:24:45 -0700

    use inner join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 -- First column must be called genre
 -- Second column must be called number_of_shows
 SELECT s.name AS genre, COUNT(g.genre_id) AS number_of_shows
-FROM tv_genres AS s LEFT JOIN tv_show_genres AS g
+FROM tv_genres AS s INNER JOIN tv_show_genres AS g
 ON s.id = g.genre_id
 GROUP BY s.name
 ORDER BY number_of_shows DESC;
