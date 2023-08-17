2b785858ae88b4a438129c6385b19acdb5ed321d
Anzelah <jepkoechanzellah@gmail.com>
2023-08-17 07:41:30 -0700

    use minus

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- list all genres not linked to the show Dexter
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+SELECT  g.name FROM tv_genres AS g
+MINUS
+SELECT g.name FROM tv_genres AS g
+INNER JOIN tv_show_genres AS sg
+ON g.id = sg.genre_id
+
+INNER JOIN tv_shows AS s
+ON s.id = sg.show_id
+WHERE s.title = 'Dexter'
+ORDER BY g.name;
