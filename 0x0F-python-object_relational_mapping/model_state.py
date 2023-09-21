e912b39ef23552ec924e3bc20a9e51ed7151f655
Anzelah <jepkoechanzellah@gmail.com>
2023-09-21 15:04:39 -0700

    create first model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Import modules"""
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class State(Base):
     """Create a new class"""
-    ___tablename__ = "states"
+    __tablename__ = "states"
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False) 
 
