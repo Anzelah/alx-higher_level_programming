83108f12c671abbc9142a9ee5c0ce761769aa2a5
Anzelah <jepkoechanzellah@gmail.com>
2023-09-21 07:00:53 -0700

    use db_name instead

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 def list_states(username, password, db_name):
     """get all the states from the database"""
     db = MySQLdb.connect(host="localhost", user=username, passwd=
-                         password, db="hbtn_0e_0_usa", port=3306)
+                         password, db=db_name, port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     results = cur.fetchall()
