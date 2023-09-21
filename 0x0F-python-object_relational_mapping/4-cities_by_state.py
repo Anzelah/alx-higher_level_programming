d23c9cfedc308e79f3d684f8202d78b4f62bb5bc
Anzelah <jepkoechanzellah@gmail.com>
2023-09-21 13:18:56 -0700

    print cities by state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Import modules"""
+import MySQLdb
+import sys
+
+def list_states(username, password, db_name):
+    """get all the states from the database"""
+    db = MySQLdb.connect(host="localhost", user=username, passwd=
+                         password, db=db_name, port=3306)
+
+    cur = db.cursor()
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states
+        ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """)
+    results = cur.fetchall()
+    for res in results:
+        print(res)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    list_states(username, password, db_name)
+    sys.exit(0)
