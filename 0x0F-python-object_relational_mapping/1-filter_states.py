9a4427a951c2d2b01a1b67ef74dd418e4f4e085f
Anzelah <jepkoechanzellah@gmail.com>
2023-09-21 07:35:29 -0700

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Import modules"""
+import MySQLdb
+import sys
+
+def filter_states(username, password, db_name):
+    """get all the states from the database"""
+    db = MySQLdb.connect(host="localhost", user=username, passwd=
+                         password, db=db_name, port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT DISTINCT * FROM states WHERE NAME LIKE 'N%' ORDER BY states.id ASC")
+    results = cur.fetchall()
+    for res in results:
+        print(res)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    filter_states(username, password, db_name)
+    sys.exit(0)
