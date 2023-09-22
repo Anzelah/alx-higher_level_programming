a262cc9ca91d02e97431f26f4c7cd9e0ab8568ec
Anzelah <jepkoechanzellah@gmail.com>
2023-09-22 16:11:56 -0700

    not working

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,17 +2,16 @@
 """Start link class to table in database
 """
 import sys
-from model_state import Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 from model_city import City
-#from sqlalchemy.orm import relationship
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-#    State.cities = relationship("City", back_populates="states")
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, db_name),
                            pool_pre_ping=True)
@@ -20,6 +19,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    results = session.query(State, City).filter(State.id == City.state_id).order_by(City.id).all()
-    for r in results:
-        print("{}: ({}) {}" .format(r[0].name, r[1].id, r[1].name))
+    results = session.query(State).join(City).order_by(City.id).all()
+    print(results[1].name)
+#    for city, state in results:
+#       print("{}: ({}) {}" .format(state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,9 +2,10 @@
 """Import modules"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-#from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
 
 class City(Base):
     """Create a new class"""
@@ -12,5 +13,4 @@ class City(Base):
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id', nullable=False))
-
 #    state = relationship("State", back_populates="cities")
