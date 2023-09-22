68bbac6c49f1da30d2a4a38c296223f6d8d3fa8a
Anzelah <jepkoechanzellah@gmail.com>
2023-09-22 16:20:51 -0700

    attempting advanced

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    results = (session.query(State).order_by(State.id).all())
+    for r in results:
+        print("{}: {}" .format(r.id, r.name))
+        for s in r.cities:
+            print("\t{}: {}" .formats.id, s.name))
