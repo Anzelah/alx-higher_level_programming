3d2677ae7898cede0b4c187a4593efb7c0e1ec86
Anzelah <jepkoechanzellah@gmail.com>
2023-09-29 06:54:40 -0700

    needed to type the body response format myself

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,5 +3,10 @@
 
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-    page = res.read()
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        page = res.read()
+    print("Body response:")
+    print("\t- type: {}" .format(type(page)))
+    print("\t- content: {}" .format(page))
+    print("\t- utf8 content: {}" .format(page.decode("utf-8")))
