6993db7d234c92c4f99ec76277a5af27882d2941
Anzelah <jepkoechanzellah@gmail.com>
2023-09-29 08:07:26 -0700

    pycodestyle update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = {"email": email} #dict with data to be sent with post request
+    data = {"email": email}  # dict with data to be sent with post request
     email = urllib.parse.urlencode(data).encode('ascii')
     req = urllib.request.Request(url, email)
     with urllib.request.urlopen(req) as res:
