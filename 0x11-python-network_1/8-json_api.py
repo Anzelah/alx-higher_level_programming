3342c82d460d77f50c74e932a320f0f659f39012
Anzelah <jepkoechanzellah@gmail.com>
2023-09-30 03:52:33 -0700

    attemting task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a module"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    data = {"q": letter}
+    res = requests.post(url, data)
+
+    try:
+        req = res.json()
+        if req:
+            print("[{}}] {}" .format(req["id"], req["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
