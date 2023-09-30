6b38e190a75fa36540cbbec5fb875f9e379eab2b
Anzelah <jepkoechanzellah@gmail.com>
2023-09-30 10:38:25 -0700

    its not working still

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,21 +5,23 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
+    url = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) == 2:
-        letter = sys.argv[1]
+        q = sys.argv[1]
     else:
-        letter = ""
+        q = ""
 
-    q = {"q": letter}
-    res = requests.post(url, q)
+    payload = {"q": q}
+    res = requests.post(url, data=payload)
 
     try:
         req = res.json()
-        if req:
-            print("[{}}] {}" .format(req["id"], req["name"]))
-        else:
+        if len(req) == 0:
             print("No result")
-    except:
+            sys.exit()
+        m_id = req.get('id')
+        m_name = req.get('name')
+        print("[{}}] {}" .format(m_id, m_name))
+    except ValueError:
         print("Not a valid JSON")
 
