2414fa5297dc671fd469711616b8329888b4b1cb
Anzelah <jepkoechanzellah@gmail.com>
2023-10-24 10:48:13 -0700

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-let filePath = process.argv[2];
 
+let filePath = process.argv[2];
 const fs = require('fs');
 
-fs.readFile('filePath', 'utf-8',  (err, data) => {
+fs.readFile('filePath', 'utf8', (err, data) => {
 	if (err) {
-		console.error('doesntexist');
 		console.error(err);
 		return;
 	}
