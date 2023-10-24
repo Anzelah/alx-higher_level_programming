4b3d2262a6c54a6b72681d62cc023cd534845c5d
Anzelah <jepkoechanzellah@gmail.com>
2023-10-24 10:57:47 -0700

    semistandard update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-let filePath = process.argv[2];
+const filePath = process.argv[2];
 const fs = require('fs');
 
-fs.readFile('filePath', 'utf8', function (err, data) => {
-	if (err) {
-		console.error(err);
-	}
-	console.log(data);
+fs.readFile(filePath, 'utf8', function (err, data) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
