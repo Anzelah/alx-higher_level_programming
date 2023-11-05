f308ff9e3823b9e284ab69bb0293b346d958bcc7
Anzelah <jepkoechanzellah@gmail.com>
2023-11-05 02:47:40 -0800

    added all tasks

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,10 @@
-document.querySelector('header').style.color = '#FF0000';
+document.addEventListener("DOMContentLoaded", () => {
+  let url = 'https://www.fourtonfish.com/hellosalut/hello/';
+  let langCode = $('#language_code').val();
+
+  $('#btn_translate').click(function () {
+    $.get(url + { langCode }, function(data) {
+      $('#hello').html(data['hello']);
+    });    
+  });
+});
